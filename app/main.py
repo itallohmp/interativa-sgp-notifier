79e0ccc500cfc0d18c29950a33cc0534874b0cff
Itallo Polito <itallomp@hotmail.com>
2026-04-22 15:54:20 -0300

    atualização swag

--- app/main.py
+++ app/main.py
@@ -2,7 +2,31 @@ from fastapi import FastAPI
 from app.services.occurrence_service import OccurrenceService
 from app.scheduler import iniciar_scheduler
 
-app = FastAPI(title="Interativa Ocorrências API")
+description = """
+## Sistema de Notificação de Ocorrências SGP
+
+API desenvolvida para integrar o **SGP** ao **Telegram**, com o objetivo de consultar ocorrências em aberto e enviar resumos automáticos para a equipe técnica.
+
+### Funcionalidades principais
+- Consultar ocorrências em aberto no SGP
+- Filtrar ocorrências do dia
+- Formatar os dados para leitura rápida
+- Enviar mensagens automaticamente para grupo técnico no Telegram
+- Possibilitar testes manuais via endpoints da API
+"""
+
+app = FastAPI(
+    title="Interativa Ocorrências API",
+    description=description,
+    version="1.0.0",
+    contact={
+        "name": "Itallo Polito",
+        "url": "https://itallohmp.pythonanywhere.com/",
+    },
+    license_info={
+        "name": "Uso Interno",
+    },
+)
 
 service = OccurrenceService()
 scheduler = iniciar_scheduler()
