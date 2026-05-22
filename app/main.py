b6e13994fe482563af313ea709370f06457f28c8
Itallo Polito <itallomp@hotmail.com>
2026-05-22 15:28:17 -0300

    settings.update

--- app/main.py
+++ app/main.py
@@ -30,7 +30,7 @@ def home():
     return {"message": "API online"}
 
 
-@app.get("/ocorrencias/abertas")s
+@app.get("/ocorrencias/abertas")
 def listar_ocorrencias_abertas():
     return service.listar_ocorrencias_abertas_do_dia()
 
