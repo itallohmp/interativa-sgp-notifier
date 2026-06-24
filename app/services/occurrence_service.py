00ae5c92bcd50d19315333fea5cba11bd20e90eb
Itallo Polito <itallomp@hotmail.com>
2026-06-24 14:17:36 -0300

    main-update

--- app/services/occurrence_service.py
+++ app/services/occurrence_service.py
@@ -1,17 +1,18 @@
+import time
+
+import httpx
+from fastapi import BackgroundTasks
+
+from app.config import settings
 from app.integrations.sgp_client import SGPClient
 from app.integrations.telegram_client import TelegramClient
 from app.utils.formatter import formatar_ocorrencias
-from app.config import settings
-from fastapi import BackgroundTasks
-import time
-import httpx
 
 
 class OccurrenceService:
     def __init__(self):
         self.sgp_client = SGPClient()
         self.telegram_client = TelegramClient()
-
 
     def listar_ocorrencias_abertas_do_dia(self) -> list:
         return self.sgp_client.listar_ordens_servico_do_dia()
@@ -21,14 +22,20 @@ class OccurrenceService:
 
     def listar_ocorrencias_amanha(self) -> list:
         return self.sgp_client.listar_ordens_amanha()
-            
+
     def enviar_ocorrencias_abertas_telegram(self):
         ocorrencias = self.listar_ocorrencias_abertas_do_dia()
         mensagem = formatar_ocorrencias(ocorrencias, periodo="hoje")
         return self.telegram_client.enviar_mensagem(mensagem)
 
-    def enviar_ocorrencias_para_chat(self, chat_id: int | str, periodo: str, user_id: int | str = None, user_name: str | None = None):
-        
+    def enviar_ocorrencias_para_chat(
+        self,
+        chat_id: int | str,
+        periodo: str,
+        user_id: int | str = None,
+        user_name: str | None = None,
+    ):
+
         if periodo == "hoje":
             ocorrencias = self.listar_ocorrencias_abertas_do_dia()
         elif periodo == "d7":
@@ -38,18 +45,18 @@ class OccurrenceService:
         else:
             raise ValueError(f"Período inválido: {periodo}")
         inicio = time.time()
-        mensagem = formatar_ocorrencias(ocorrencias, periodo=periodo, user_id=user_id, user_name=user_name)
+        mensagem = formatar_ocorrencias(
+            ocorrencias, periodo=periodo, user_id=user_id, user_name=user_name
+        )
         fim = time.time()
         print(f"Rota inteira demorou: {fim - inicio:.2f} segundos")
         return self.telegram_client.enviar_mensagem_para(chat_id, mensagem)
-        
-    
+
     def configurar_webhook(self):
-        webhook_url = f"{settings.PUBLIC_URL}/interativa-api/webhook"
+        webhook_url = f"{settings.PUBLIC_URL}/webhook"
 
         url = (
-            f"https://api.telegram.org/bot"
-            f"{settings.TELEGRAM_BOT_TOKEN}/setWebhook"
+            f"https://api.telegram.org/bot" f"{settings.TELEGRAM_BOT_TOKEN}/setWebhook"
         )
 
         response = httpx.post(url, json={"url": webhook_url})
@@ -65,13 +72,12 @@ class OccurrenceService:
             data = callback["data"]
             chat_id = callback["message"]["chat"]["id"]
             user_id = callback["from"]["id"]
-            user_name = (callback["from"].get("first_name")
-                or callback["from"].get("username")
+            user_name = callback["from"].get("first_name") or callback["from"].get(
+                "username"
             )
 
             self.telegram_client.answer_callback_query(
-                callback_id,
-                texto="Buscando OS..."
+                callback_id, texto="Buscando OS..."
             )
 
             mapa_periodos = {
@@ -88,7 +94,7 @@ class OccurrenceService:
                     chat_id,
                     periodo,
                     user_id,
-                    user_name
+                    user_name,
                 )
 
             return {"ok": True}
@@ -100,11 +106,10 @@ class OccurrenceService:
 
             if texto in ("/menu", "/os"):
 
-                background.add_task(
-                    self.telegram_client.enviar_menu,
-                    chat_id=chat_id
-                )
+                background.add_task(self.telegram_client.enviar_menu, chat_id=chat_id)
 
             return {"ok": True}
 
+        return {"ok": True}
+        return {"ok": True}
         return {"ok": True}
